6e46df92b23d1d838a537376b309cd9cd76f762c
imtiaz69 <imtiazahmed101567@gmail.com>
2025-12-06 02:41:16 +0600

    server config setup

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+// import petRoutes from "./routes/petRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
+
+const MONGO_URI = process.env.MONGO_URI;
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB Atlas connected");
+  } catch (err) {
+    console.error("Connection failed:", err.message);
+    
+  }
+};
+
+connectDB();
+
+// app.use("/api/user", userRoutes);
+// app.use("/api/pet", petRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
